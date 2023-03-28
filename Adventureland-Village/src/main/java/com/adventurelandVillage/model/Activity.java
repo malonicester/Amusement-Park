22f1af21a19b69612beec547b7cec60925e4833b
ashishtripathy39 <ashish.tripathy39@gmail.com>
2023-03-28 16:15:23 +0530

    Models Created

--- Adventureland-Village/src/main/java/com/adventurelandVillage/model/Activity.java
+++ Adventureland-Village/src/main/java/com/adventurelandVillage/model/Activity.java
@@ -1,26 +1,21 @@
 package com.adventurelandVillage.model;
 
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-@Entity
-public class Customer extends AbstractUser {
+@Data
+public class Activity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long customerId;
-	@OneToMany(mappedBy = "customers")
-	private List<Ticket> ticketes;
+	private Long activityId;
+	private String description;
+	private float charges;
 }
