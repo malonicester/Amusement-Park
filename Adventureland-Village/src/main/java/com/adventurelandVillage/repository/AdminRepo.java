423934c0ea8837496da0f178e8539c540412da8d
ashishtripathy39 <ashish.tripathy39@gmail.com>
2023-03-29 19:50:18 +0530

    Daily SUbmission

--- Adventureland-Village/src/main/java/com/adventurelandVillage/repository/AdminRepo.java
+++ Adventureland-Village/src/main/java/com/adventurelandVillage/repository/AdminRepo.java
@@ -1,16 +1,12 @@
 package com.adventurelandVillage.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.adventurelandVillage.model.Activity;
 import com.adventurelandVillage.model.Admin;
-import com.adventurelandVillage.model.Customer;
-@Repository
-public interface AdminRepo extends JpaRepository<Admin, Long>  {
-public Admin findByUserName(String userName);
 
+@Repository
+public interface AdminRepo extends JpaRepository<Admin, Long> {
+	public Admin findByUserName(String userName);
 
 }
