eb708ff783ef63af897d1b8bb120db1d1cef214c
ashishtripathy39 <ashish.tripathy39@gmail.com>
2023-03-29 11:51:05 +0530

    Validation

---
+++ Adventureland-Village/src/main/java/com/adventurelandVillage/controller/AdminController.java
@@ -0,0 +1,9 @@
+package com.adventurelandVillage.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+   
+	
+}
---
+++ Adventureland-Village/src/main/java/com/adventurelandVillage/repository/CurrenUserSessionRepository.java
@@ -0,0 +1,11 @@
+package com.adventurelandVillage.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adventurelandVillage.model.CurrentUserSession;
+
+@Repository
+public interface CurrenUserSessionRepository extends JpaRepository<CurrentUserSession, Long	> {
+		public CurrentUserSession findByUuid(String uuid);
+}
