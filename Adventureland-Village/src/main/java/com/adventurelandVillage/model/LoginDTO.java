011ce7cfd09fa2d5a30c590c90cd2e37fd2ac889
ashishtripathy39 <ashish.tripathy39@gmail.com>
2023-04-02 16:47:03 +0530

    changes

--- Adventureland-Village/src/main/java/com/adventurelandVillage/model/LoginDTO.java
+++ Adventureland-Village/src/main/java/com/adventurelandVillage/model/LoginDTO.java
@@ -1,10 +1,12 @@
 package com.adventurelandVillage.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class LoginDTO {
 	private String userName;
 	private String password;
+	@NotNull
 	private Role role;
 }
